8a82156519fdde47f7c2fc8c4a3f34ce08b653a1
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 07:57:44 +0530

    Add files via upload

---
+++ Excersice_1.java
@@ -0,0 +1,13 @@
+public class Excersice_1 {
+    public static void main(String[] args) {
+        int Num= 13;
+        int Number=(Num% 2);
+
+        if (Number==0){
+            System.out.println("This is a even number");
+        }
+        else {
+            System.out.println("This is a odd number");
+        }
+    }
+}
