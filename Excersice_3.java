9adcf303acff216cbc9ddcd27aa1380c40b8446e
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 07:58:37 +0530

    Add files via upload

---
+++ Excersice_3.java
@@ -0,0 +1,13 @@
+public class Excersice_3 {
+    public static void main(String[] args){
+        char ch ='A';
+
+        if (ch == 'A'|| ch == 'E' || ch == 'I'|| ch == 'O' || ch == 'U' ){
+            System.out.println("This is vowel");
+        }
+        else {
+            System.out.println("This is Consonant");
+        }
+    }
+
+}
