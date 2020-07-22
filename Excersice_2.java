8834d1b04697a733d941e8df63ddaac444780fc6
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 07:58:14 +0530

    Add files via upload

---
+++ Excersice_2.java
@@ -0,0 +1,12 @@
+public class Excersice_2 {
+    public static void main(String[] args) {
+        int Year=2000;
+
+        if (Year % 4==0){
+            System.out.println("This is leap year");
+        }
+        else{
+            System.out.println("This is not leap year");
+        }
+    }
+}
