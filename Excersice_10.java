cc2e0a25cd9187f201857d9df31e25be3a1d5f7e
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 09:03:01 +0530

    Add files via upload

---
+++ Excersice_10.java
@@ -0,0 +1,22 @@
+public class Excersice_10 {
+    public static void main(String[] args) {
+
+        int num = 43;
+        int  count = 0;
+
+        for ( int i = 2; i<=num/2; ++i){
+            if (num % i ==0){
+                count++;
+            }
+
+        }
+
+        if (count==0){
+            System.out.println(num + " is a prime number");
+        }
+        else {
+            System.out.println(num + " is  not a prime number");
+        }
+
+    }
+}
