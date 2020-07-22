b68af6343810303936fe68c0073830259cc2f99d
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 07:59:41 +0530

    Add files via upload

---
+++ Excersice_5.java
@@ -0,0 +1,14 @@
+public class Excersice_5 {
+    public static void main(String[] args) {
+        int X=2;
+        int Y=5;
+        int Power = 1;
+
+        for (int i=1; i<=Y; i++){
+            Power*= X;
+        }
+
+        System.out.println(Power);
+    }
+
+}
