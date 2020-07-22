6619456d0fa91b138fe5da46de82d3e877f366fe
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 09:02:04 +0530

    Add files via upload

---
+++ Excersice_8.java
@@ -0,0 +1,11 @@
+public class Excersice_8 {
+    public static void main(String[] args){
+        int[]  array ={5,10,15,20,25};
+        int sum = 0;
+
+        for (int num : array) {
+            sum = sum+num;
+        }
+         System.out.println("Sum array element is :" + sum);
+    }
+}
