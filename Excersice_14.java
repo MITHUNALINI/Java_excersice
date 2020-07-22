a9864af832dc37b9dd62b5fb6e108e7ff67b26b2
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 09:09:53 +0530

    Add files via upload

---
+++ Excersice_14.java
@@ -0,0 +1,11 @@
+public class Excersice_14 {
+    public static void main(String[] args) {
+        int num=10;
+        int ans=1;
+
+        for(int i=1; i<=num; i++){
+            ans= ans*i;
+        }
+        System.out.println(ans);
+    }
+}
