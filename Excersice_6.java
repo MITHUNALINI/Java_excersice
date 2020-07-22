c6449d4909f42e99c246404f1e497522b9990c8c
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 09:01:16 +0530

    Add files via upload

---
+++ Excersice_6.java
@@ -0,0 +1,10 @@
+public class Excersice_6 {
+    public static void main(String[] args) {
+        String str= "Mithunalini";
+
+        for (int startName= str.length()-1; startName>=0; startName--){
+            System.out.print(str.charAt(startName));
+        }
+
+    }
+}
