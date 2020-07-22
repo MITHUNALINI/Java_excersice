a48a1394c5ef9fac8f4e0bdd1ad3025f88b5dbda
MITHUNALINI <65950798+MITHUNALINI@users.noreply.github.com>
2020-07-22 07:59:07 +0530

    Add files via upload

---
+++ Excersice_4.java
@@ -0,0 +1,12 @@
+public class Excersice_4 {
+    public  static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 3;
+
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+}
